f4e738d5dfd983b196674ef76a1fc752ee09e7c2
le-nn <renpei.okada@outlook.com>
2022-11-09 11:28:56 +0900

    Remove unnecessary files and refactor.

--- git-le-nn.bat
+++
@@ -1,4 +0,0 @@
-git config --local  user.name "le-nn"
-git config --local  user.email  "renpei.okada@outlook.com"
-git config --local credential.helper wincred
-git config --local --list
--- src/defaultSettingsJson.ts
+++ src/defaultSettingsJson.ts
@@ -1,4 +1,4 @@
-export const defaultSettings =`
+export const defaultSettings = `
 {
     "isFoodEnabled": true,
     "isRippleEnabled": true,
